2f07753afa2076b034ce564aeb724caef896908b
Alex Perez Garcia <aperezgdev@gmail.com>
2024-12-25 13:50:42 +0100

    feat: add validation errors for value objects

---
+++ internal/domain/errors/validation_errors.go
@@ -0,0 +1,28 @@
+package errors
+
+import "fmt"
+
+var (
+	FieldRequired = func(field string) ValidationError {
+		return New(field, "Is required")
+	}
+	OutRange = func(field string, min, max uint8) ValidationError {
+		return New(field, fmt.Sprintf("Length should be between %d and %d ", min, max))
+	}
+	FormatInvalidad = func(field string) ValidationError {
+		return New(field, "Format is not valid")
+	}
+)
+
+type ValidationError struct {
+	Field string
+	Message string
+}
+
+func New(field, message string) ValidationError {
+	return ValidationError{field, message}
+}
+
+func (v ValidationError) Error() string {
+	return fmt.Sprintf("%s: %s", v.Field, v.Message)
+}
